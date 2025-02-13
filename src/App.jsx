080805a1b9ceac33ec71c5c9782ca6f90ad05f71
Kittithat-L <kittithat.git@gmail.com>
2025-02-13 23:57:49 +0700

    Main

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { IoRoseOutline } from "react-icons/io5";
+function App() {
+
+  return (
+    <div className="container">
+      <p className='header'><IoRoseOutline />Happy Valentine Day<IoRoseOutline /></p>
+    </div>
+  )
+}
+
+export default App
